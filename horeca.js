c5b1eebb2d54ce03a07f51029244134c72053422
Gianni Ridwan <99036552@mydavinci.nl>
2020-12-08 13:13:19 +0100

    Opdracht afgemaakt

--- horeca.js
+++ horeca.js
@@ -1,23 +1,69 @@
 const fris = Number(2.50);
 const bier = Number(2.00);
 const wijn = Number(3.00);
-var orderQty = Number(0);
+const snacks8 = Number(5.00);
+const snacks16 = Number(8.00);
+
+var frisQty = Number(0);
+var bierQty = Number(0);
+var wijnQty = Number(0);
+var achtBal = Number(0);
+var zestienBal = Number(0);
 
 function fullOrder(){
-	var order = prompt("Welke bestelling wilt u toevoegen? Kies uit 'fris', 'bier' of 'wijn'.");
+	var order = prompt("Welke bestelling wilt u toevoegen? Kies uit 'fris', 'bier', 'wijn' of 'snacks'.");
 		if (order == "stop"){
-			document.write(orderQty + "x " + order + "<br>");
-			document.write("Totaal: " + (order * orderQty));
+			if (frisQty > 0){
+				document.write(frisQty + "x fris" + "<br>");
+			}
+			if (bierQty > 0){
+				document.write(bierQty + "x bier" + "<br>");
+			}
+			if (wijnQty > 0){
+				document.write(wijnQty + "x wijn" + "<br>");
+			}
+			if (achtBal > 0){
+				document.write(achtBal + "x 8 snacks" + "<br>");
+			}
+			if (zestienBal > 0){
+				document.write(zestienBal + "x 16 snacks" + "<br>");
+			}
+			
+			document.write("Totaal: " + ((fris * frisQty) + (bier * bierQty) + (wijn * wijnQty) + (achtBal * snacks8) + (zestienBal * snacks16)));
 		} 
 
-		else if (order == "fris" || order == "bier" || order == "wijn"){
-			orderQty = prompt("Hoeveel " + order + " wilt u toevoegen aan uw bestelling?");
+		else if (order == "fris"){
+			frisQty = prompt("Hoeveel fris wilt u toevoegen aan uw bestelling?");
 			fullOrder();
-		} 
+		}
+
+		else if (order == "bier"){
+			bierQty = prompt("Hoeveel bier wilt u toevoegen aan uw bestelling?");
+			fullOrder();
+		}
+
+		else if (order == "wijn"){
+			wijnQty = prompt("Hoeveel wijn wilt u toevoegen aan uw bestelling?");
+			fullOrder();
+		}
+
+		else if (order == "snacks"){
+			snacks = prompt("Hoeveel bitterballen wilt u toevoegen (8 of 16)?");
+			if (snacks == 8){
+				achtBal = prompt("Hoeveel bitterbalschalen van 8 stuks wilt u bestellen?");
+			}
+			else if (snacks == 16){
+				zestienBal = prompt("Hoeveel bitterbalschalen van 16 stuks wilt u bestellen?");
+			}
+			else{
+				alert("U kunt alleen een keuze maken tussen 8 en 16. De snacks zijn niet toegevoegd aan de bestelling.");
+			}
+			fullOrder();
+		}
 
 		else {
 			alert("U heeft een ongeldige invoer gedaan. Uw bestelling kan niet worden toegevoegd.");
 			fullOrder();
 		}
 }		
-fullOrder();	
+fullOrder();
